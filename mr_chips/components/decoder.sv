923b7e59851cd7bffc4a15fbac8aebf9d34095a6
MeghanRN <meiranotkin@gmail.com>
2023-05-09 19:43:03 -0500

    fixed the instruction decoder

--- mr_chips/components/decoder.sv
+++ mr_chips/components/decoder.sv
@@ -1,33 +1,35 @@
 `ifdef decoder
-def decoder
 
 module decoder (
-input [15:0] instruction,
-output reg [3:0] opcode
+    input [15:0] instruction,
+    output reg [3:0] opcode
 );
 
-always @ (instruction) begin
- case (instruction[15:12])
-     4'b0000: opcode = 4'b0000; // R-type instruction
-     4'b0001: opcode = 4'b0001;
-     4'b0010: opcode = 4'b0010;
-     4'b0011: opcode = 4'b0011;
-     4'b0100: opcode = 4'b0100;
-     4'b0101: opcode = 4'b0101;
-     4'b0110: opcode = 4'b0110;
-     4'b0111: opcode = 4'b0111;
-     4'b1000: opcode = 4'b1000;
-     4'b1001: opcode = 4'b1001;
-     4'b1010: opcode = 4'b1010;
-     4'b1011: opcode = 4'b1011;
-     4'b1100: opcode = 4'b1100;
-     4'b1101: opcode = 4'b1101;
-     4'b1110: opcode = 4'b1110;
-     4'b1111: opcode = 4'b1111;
-     default: opcode = 4'bxxxx;
- endcase
-end
-
+    always @ (opcode) begin
+        case (opcode)
+            4'b0000: begin // If opcode 0000, it's an R-type instruction
+                Rs <= instuction[8:6];
+                Rt <= instruction[5:3];
+                Rd <= instruction[2:0];
+                funct <= instruction[11:9];
+            end
+            4'b0001, // if 0001-0101 then I
+            4'b0010,
+            4'b0011,
+            4'b0100,
+            4'b0101,
+            4'b0110:  begin
+                imm <= instruction[11:6];
+                Rt <= instruction[5:3];
+                Rd <= instruction[2:0];
+            end
+            4'b0111,
+            4'b1000,
+            4'b1001: address <= inst[11:0];
+            default: Rs, Rt, Rd, funct, imm <= 0; // If the first 4 bits don't match any opcode, set typo to 'x'
+        endcase
+    end
 
 endmodule
+
 `endif //decoder
